226c1a8f0208120a0e1386c0583d2d21168c9ca1
Urs Grupp <urs.grupp@gmail.com>
2017-12-12 10:38:09 +0100

    Added smoothscroll.

--- _js/main.js
+++ _js/main.js
@@ -4,3 +4,4 @@ import 'babel-polyfill';
 import './modules/splash';
 import './modules/scrollFlag';
 import './modules/masonry';
+import './modules/smoothScroll';
---
+++ _js/modules/smoothScroll.js
@@ -0,0 +1,4 @@
+import SmoothScroll from 'smooth-scroll/dist/js/smooth-scroll.js';
+
+// activate for all anchor links
+let scroll = new SmoothScroll('a[href*="#"]');
