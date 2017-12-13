ac13ff3fdaa0d99a298e297dd92a1dcd2a0fee20
Urs Grupp <urs.grupp@gmail.com>
2017-12-13 23:56:28 +0100

    Disable scrollreveal on mobile.

--- _js/modules/scrollreveal.js
+++ _js/modules/scrollreveal.js
@@ -11,6 +11,7 @@ sr.reveal('[data-reveal]', {
       duration: 750,
       distance: '3em',
       scale: 1,
+      mobile: false,
       //easing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)', // ease-out-back
       afterReveal: function () {}
     })
