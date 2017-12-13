5b61fad16506bd6f791953e4b6ee6f67eafc4c5e
Urs Grupp <urs.grupp@gmail.com>
2017-12-13 16:01:58 +0100

    BRAG MORE

--- _js/modules/brag.js
+++ _js/modules/brag.js
@@ -1,4 +1,4 @@
-let bragMsg = 'Coded by Urs Grupp - https://www.21sieben.de ';
+let bragMsg = 'Coded with 💖 by Urs Grupp - https://www.21sieben.de ';
 if (typeof InstallTrigger !== 'undefined' || (!!window.chrome && !!window.chrome.webstore)) {
   let bragMsgFormatted = ['%c ' + bragMsg, 'display:block; padding:5px; background: #303e43; line-height:40px; color:#fff;'];
   window.console.log.apply(console, bragMsgFormatted);
