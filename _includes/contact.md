0cacce7585704d41559dda1abead96e7c68c38c6
Urs Grupp <urs.grupp@gmail.com>
2017-12-14 14:31:01 +0100

    ScrollReveal linting, wording changes.

--- _includes/contact.md
+++ _includes/contact.md
@@ -1,3 +1,3 @@
-I'm currently **available for hire**. I like working on web projects of any size and complexity, preferably doing remote work in an agile environment. That being said, of course, I'm totally cool with traveling from time to time.
+I'm currently **available for hire**. I like working on web projects of any size and complexity, preferably doing remote work in an agile environment. That being said, of course, I'm totally fine with traveling from time to time.
 
 If you're interested in working together or would like to learn more about me and my work, just drop a line and we'll talk details:
--- _js/modules/scrollreveal.js
+++ _js/modules/scrollreveal.js
@@ -1,19 +1,18 @@
-import ScrollReveal from 'scrollreveal';
+import scrollReveal from 'scrollreveal';
 
-let sr = ScrollReveal();
+let sr = scrollReveal();
 
 // Disable reveal on pageload
 // https://github.com/jlmakes/scrollreveal/issues/276
 sr.reveal('[data-reveal]', {
   duration: 0,
-  afterReveal: function () {
+  afterReveal() {
     sr.reveal('[data-reveal]', {
       duration: 750,
       distance: '3em',
       scale: 1,
       mobile: false,
-      //easing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)', // ease-out-back
-      afterReveal: function () {}
-    })
-  }
+      afterReveal() {},
+    });
+  },
 });
